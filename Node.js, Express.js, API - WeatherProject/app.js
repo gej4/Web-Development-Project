c7c5e9b431b4db8f23a5269d33452b8beeb70fd8
gej4 <gej4@uci.edu>
2020-08-22 20:10:34 -0400

    Create and finish simple website project to fetch dynamic data using OpenWeatherAPI, and use express.js to ask for the city name input and display current weather and weather icon

---
+++ Node.js, Express.js, API - WeatherProject/app.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const https = require('https');
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/", function(req, res){
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function(req, res){
+  const query = req.body.cityName;
+  const apiKEY = "ec9ccb650e73f13290fbeca72977d260";
+  const units = "metric";
+  const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apiKEY + "&units=" + units;
+  https.get(url, function(response){
+
+    response.on("data", function(data){
+      const weatherData = JSON.parse(data);
+      const temp = weatherData.main.temp;
+      const weatherDescription = weatherData.weather[0].description;
+      const icon = weatherData.weather[0].icon;
+      const iconUrl = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
+
+      // There can be multiple res.write, but only one res.send in one GET.
+      res.write("<p>The weather is currently " + weatherDescription + "</p>");
+      res.write("<h1>The temperature in " + query + " is " + temp + " degrees Celcius.</h1>");
+      res.write("<img src='" + iconUrl + "' alt=''>")
+      res.send();
+    })
+
+  })
+});
+
+
+
+
+
+app.listen(3000, function(req, res){
+  console.log("Server is running on port 3000.")
+});
