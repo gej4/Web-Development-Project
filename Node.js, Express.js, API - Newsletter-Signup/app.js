3effb4fb361e1d6c705eb4e162ffeb91d3392ba5
gej4 <gej4@uci.edu>
2020-08-24 02:59:11 -0400

    change to fit the APP running on Heroku server

--- Node.js, Express.js, API - Newsletter-Signup/app.js
+++ Node.js, Express.js, API - Newsletter-Signup/app.js
@@ -68,6 +68,6 @@ app.post("/failure", function(req,res){
 })
 
 
-app.listen(3000, function(req, res){
+app.listen(process.env.PORT || 3000, function(req, res){
   console.log("Server is running on port 3000.")
 });
